9ff77d63837e1a53f26ee8ce16fd5072570a7e97
Kosta <kostadink.tsi@educa.jcyl.es>
2020-02-06 18:42:49 +0100

    Version 1.0 del proyecto de rutas

---
+++ precache-manifest.633a137eea001e03be4bf56393762d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd61534a26537e64fbdbb3eaa3e0931",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39e33c1b9e4989ddac46",
+    "url": "./static/js/2.5396f74f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.5396f74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e141146cf6e93b5682",
+    "url": "./static/js/main.fc6e7bb8.chunk.js"
+  },
+  {
+    "revision": "5189b89ea19571993054",
+    "url": "./static/js/runtime-main.12d922d1.js"
+  }
+]);
